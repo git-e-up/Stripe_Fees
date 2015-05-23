3a34e63c2d1649d5205ed866cd097c248f46788e
Matt Lissner <mlissne@gmail.com>
2015-05-23 14:58:02 -0500

    search algorithm in model

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,33 @@
 class Search < ActiveRecord::Base
+  def range(start_date, end_date)
+
+    fees_in_range = []
+
+    start_date = start_date.to_time.to_i
+
+    end_date = end_date.to_time.to_i
+
+    transaction_list = Stripe::BalanceTransaction.all(:limit => 100)
+
+    fees_list  = []
+    fees_array = []
+
+    transaction_list.each do |item|
+    		fees_list.push(item)
+    end
+
+    fees_list.each do |feee|
+    	if (feee.created >= start_date && feee.created <= end_date)
+    		fees_in_range.push(feee)
+    	end
+    end
+
+    puts fees_in_range
+
+
+
+    end
+
 end
+
+# range(Date.new(2015,5,10), Date.new(2015,5,21))
